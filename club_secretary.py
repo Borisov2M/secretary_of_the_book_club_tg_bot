bf9ebf73fe8e3704a901a9bfcac65f93657e2dc2
Mikhail Borisov <101577077+Borisov2M@users.noreply.github.com>
2022-12-22 09:26:30 +0300

    Add comments

--- club_secretary.py
+++ club_secretary.py
@@ -29,16 +29,19 @@ def randomize_names(m, res=False):
             if '\n' in name:
                 name = re.sub('\n', '', name)
             name_list.append(name)
+    name_file_object.close()
 
     with open(meeting_number_path, 'r') as number_file_object:
         round_number = int(number_file_object.read())
+    number_file_object.close()
 
     message = (f'Список на {round_number} - й круг заседания' +
                      ' клуба\n')
-
     round_number += 1
+
     with open(meeting_number_path, 'w') as number_file_object:
         number_file_object.write(str(round_number))
+    number_file_object.close()
 
     random.shuffle(name_list)
     for name in name_list:
@@ -55,6 +58,8 @@ def view_member_list(m, res=False):
         for name in name_file_object:
             list_name_message += '     - ' + name
     
+    name_file_object.close()
+
     bot.send_message(m.chat.id, list_name_message)
 #-------------------------------------------------------------------------------
 
@@ -70,6 +75,7 @@ def add_neofit(m, res=False):
         with open(name_list_path, 'a') as name_file_object:
             name_file_object.write(m.text[1:] + '\n')
         bot.send_message(m.chat.id, 'Неофит добавлен в список')
+        name_file_object.close()
 #-------------------------------------------------------------------------------
 
 #------------------- Удаление из списка членов книжного клуба ------------------
@@ -82,8 +88,8 @@ def member(m, res=False):
 
 def member_delete(m, res=False):
     with open(name_list_path, 'r') as name_file_object: 
-        
         name_list = name_file_object.readlines()
+    name_file_object.close()
     delete_name = m.text[1:]
         
     with open(name_list_path, 'w') as name_file_object:
@@ -95,12 +101,14 @@ def member_delete(m, res=False):
 
             if plug != m.text[1:] :
                 name_file_object.write(name)
-    
+
+    name_file_object.close    
     bot.send_message(m.chat.id, 'Отступник стерт из хроник!')
 #-------------------------------------------------------------------------------
 #*******************************************************************************
 
 #*************************** Блотк работы с книгами ****************************
+#-------------------- Добавление новых книг книжного клуба ---------------------
 @bot.message_handler(commands=["add_book"])
 def new_book(m, res=False):
     bot.send_message(m.chat.id, "Ввод начинается с символа '/'")
@@ -112,6 +120,10 @@ def add_book_name(m, res=False):
         book_file_object.write(m.from_user.username  + ': ' + m.text[1:] + '\n')
     bot.send_message(m.chat.id,'Книга поставлена на полку')
 
+    book_file_object.close()
+#-------------------------------------------------------------------------------
+
+#------------- Удаление из списка прочитанных книг книжного клуба --------------
 @bot.message_handler(commands=["delete_book"])
 def book(m, res=False):
     bot.send_message(m.chat.id, "Ввод начинается с символа '/'")
@@ -122,6 +134,8 @@ def book(m, res=False):
 def book_delete(m, res=False):
     with open(book_list_path, 'r') as book_file_object:     
         book_list = book_file_object.readlines()
+    
+    book_file_object.close()
         
     with open(book_list_path, 'w') as book_file_object:
         for book in book_list:
@@ -133,8 +147,11 @@ def book_delete(m, res=False):
             if plug != (m.from_user.username + ': ' + m.text[1:]) :
                 book_file_object.write(book)
 
+    book_file_object.close
     bot.send_message(m.chat.id, 'Книга убрана из библиотеки!')
+#-------------------------------------------------------------------------------
 
+#--------------------- Вывод списка прочитанных книг клуба ---------------------
 @bot.message_handler(commands=["books_list"])
 def view_book_list(m, res=False):
     list_book_message = 'Список прочитанных книг:\n'
@@ -142,18 +159,24 @@ def view_book_list(m, res=False):
         for name in book_file_object:
             list_book_message += '     - ' + name
     
+    book_file_object.close()
     bot.send_message(m.chat.id, list_book_message)
+#-------------------------------------------------------------------------------
 #*******************************************************************************
 
+#---------------------- Вывод списка правил книжного клуба ---------------------
 @bot.message_handler(commands=["rule_book"])
-def view_member_list(m, res=False):
+def view_rule_book(m, res=False):
     rule_book_message = 'Список правил клуба:\n'
     with open(rule_book_path, 'r', encoding='utf-8') as rule_book_object:
         for rule in rule_book_object:
             rule_book_message += '     ' + rule
     
+    rule_book_object.close()
     bot.send_message(m.chat.id, rule_book_message)
+#-------------------------------------------------------------------------------
 
+#------------------------ Сказать привет секретарю клуба -----------------------
 @bot.message_handler(commands=["hello","привет"])
 def hello(m, res=False):
     t = time.localtime() 
@@ -169,7 +192,9 @@ def hello(m, res=False):
         message = ('Хорошая ночь, ' + str(m.from_user.username) + '!')
 
     bot.send_message(m.chat.id, message)
+#-------------------------------------------------------------------------------
 
+#---------------------- Список команд для работы секретаря ---------------------
 @bot.message_handler(commands=["help", "подсказка"])
 def help(m, res=False):
     message = '/randomize_members - жребий очередности выбора\n'
@@ -183,6 +208,7 @@ def help(m, res=False):
     message += '/hello - поздороваться\n'
     message += '/help - подсказка'
     bot.send_message(m.chat.id, message)
+#-------------------------------------------------------------------------------
 
 # Запускаем бота
 bot.polling(none_stop=True, interval=0)
